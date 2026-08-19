69b16b5bd722a694afad72864d5572edc3506aba
arpit0381 <avi.avimishra.com@gmail.com>
2026-08-19 23:16:10 +0530

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+// var a = 10
+
+// var b = 20
+
+
+// console.log(a, b)
+
+
+// variable create karne ke liye 3 types use kar sakte hai 
+
 var a = 10
+let b = 20
+const c = 30
 
-var b = 20
-
-
-console.log(a, b)
+a = 99
+b = 100
+c = 200
+console.log(a);
 
 
