7daf504dc2df012db2eae3e423596d9071b0fd5a
arpit0381 <avi.avimishra.com@gmail.com>
2026-08-19 23:31:50 +0530

    Script.js

--- script.js
+++ script.js
@@ -8,13 +8,26 @@
 
 // variable create karne ke liye 3 types use kar sakte hai 
 
-var a = 10
-let b = 20
-const c = 30
+// var a = 10
+// let b = 20
+// const c = 30
 
-a = 99
-b = 100
-c = 200
-console.log(a);
+// a = 99
+// b = 100
+// c = 200 // bacause c contains const value it can't be changed.
+// console.log(a);
 
 
+// arthematic operators
+// var a = 10
+// var b = 20
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+
+var a = "hey, I am Sarthak Sharma"
+
+console.log(a)
